e991fa2b1361a0fb9f17e0b3ec937fd9467598e0
Your Name <you@example.com>
2026-02-23 13:28:42 +0700

    add some details

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,9 +4,9 @@ import { NavLink } from "react-router";
 export default function Layout(props: PropsWithChildren): React.JSX.Element {
     return (
         <div>
-            <NavLink to="/"><button>Home</button>a</NavLink>
-            <NavLink to="/menu"><button>Menu List</button>a</NavLink>
-            <NavLink to="/menu/create"><button>Create Menu</button>a</NavLink>
+            <NavLink to="/"><button>Home</button></NavLink>
+            <NavLink to="/menu"><button>Menu List</button></NavLink>
+            <NavLink to="/menu/create"><button>Create Menu</button></NavLink>
             <br />
             <br />
             {props.children}
